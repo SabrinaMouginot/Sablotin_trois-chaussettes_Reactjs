0d53f2f6e2bd1ecebb70311d593030471c1c0de2
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-11-17 03:19:33 +0100

    Creation du Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+    return (
+      <footer style={styles.footer}>
+        <p>© 2024 - Le calendrier de l&#39;Avent de Sablotin Trois-Chaussettes</p>
+      </footer>
+    );
+  }
+  
+  const styles = {
+    footer: {
+      backgroundColor: '#B22222',
+      color: '#FFF',
+      textAlign: 'center',
+      padding: '10px',
+      marginTop: '20px',
+    },
+  };
+  
+  export default Footer;
+  
