a3bf113bfa1999f3a6dcabe1d456ed2e6027ad2c
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-11-17 03:17:24 +0100

    dans Home liens vers les anecdotes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const anecdotes = Array.from({ length: 24 }, (_, i) => ({
+  day: i + 1,
+  title: `Jour ${i + 1}`,
+}));
+
 function Home() {
   return (
     <div>
       <h1>Bienvenue sur le calendrier de l&#39;avent de Sablotin Trois-Chaussettes !</h1>
       <p>Découvrez chaque jour une anecdote hilarante en attendant Noël :</p>
+      <ul>
+        {anecdotes.map((a) => (
+          <li key={a.day}>
+            <Link to={`/anecdote/${a.day}`}>{a.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
