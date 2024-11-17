de103d07e50ad7ea27b051f28a3ffd634f13ec34
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-11-17 03:15:20 +0100

    creation de la page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur le calendrier de l&#39;avent de Sablotin Trois-Chaussettes !</h1>
+      <p>Découvrez chaque jour une anecdote hilarante en attendant Noël :</p>
+    </div>
+  );
+}
+
+export default Home;
